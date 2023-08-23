9ff23982804ec23fa666e068a42b9117362402c1
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-23 13:23:10 +0530

    destructuring arrays

--- script.js
+++ script.js
@@ -26,4 +26,22 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+// ****Destructuring Arrays****
+
+const arr = [2, 4, 6];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
