a9c5ff6b915ef64220da512de3f7c641dbfe1a99
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-25 00:38:13 +0530

    looping object for properties

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,20 +29,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // ES6 enhanced object literals
+  openingHours,
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -49,6 +55,27 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////
+// ****Looping Objects: Object Keys, Values, and Entries****
+
+// ****Property NAMES****
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days:`;
+
+for (const day of properties) {
+  openStr += `${day},`;
+}
+console.log(openStr);
+
+// ****Property VALUES****
+
+// ****Entire object****
+
+// ****[key, value]****
+
+/*
 ///////////////////////////////////////
 // ***** Optional Chaining*****
 if (restaurant.openingHours && restaurant.openingHours.mon)
@@ -77,6 +104,8 @@ console.log(user[0]?.name ?? 'User array empty');
 
 if (user.length > 0) console.log(user[0].name);
 else console.log('User array empty');
+*/
+
 /*
 ///////////////////////////////////////
 // ****The for-of Loop****
