eb85e82b4c1deaf5a1c3e61fd0b5d9838039f39c
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-28 05:26:45 +0530

    challenge#03

--- script.js
+++ script.js
@@ -56,6 +56,46 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// *****Coding Challenge #3*****
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const evnet = [...new Set(gameEvents.values())];
+console.log(evnet);
+
+// 2.
+console.log(gameEvents.delete(64));
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+/*
+///////////////////////////////////////
 //*****Maps: Iteration*****
 const question = new Map([
   ['question', 'Which is the best programming language in the world?'],
@@ -89,6 +129,7 @@ console.log([...question]);
 // console.log(question.entries());
 console.log([...question.keys()]);
 console.log([...question.values()]);
+*/
 
 /*
 ///////////////////////////////////////
