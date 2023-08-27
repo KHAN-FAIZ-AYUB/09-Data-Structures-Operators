b5eade0caeced2483924191423467d618358e19a
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-28 04:42:00 +0530

    MAP to Array

--- script.js
+++ script.js
@@ -78,12 +78,17 @@ console.log(question.get('question'));
 for (const [key, value] of question) {
   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
 }
-const answer = Number(prompt('Your Answer'));
-// const answer=3
+// const answer = Number(prompt('Your Answer'));
+const answer = 3;
 console.log(answer);
 
 console.log(question.get(question.get('correct') === answer));
+
 // *****Convert map to array*****
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
 
 /*
 ///////////////////////////////////////
