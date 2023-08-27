765058389f64dbe3701a5186f4acb39bba4a61b2
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-28 04:38:04 +0530

    Quiz App

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const question = new Map([
   [1, 'C'],
   [2, 'Java'],
   [3, 'JavaScript'],
-  ['corresct', 3],
+  ['correct', 3],
   [true, 'Correct'],
   [false, 'Try Again!'],
 ]);
@@ -72,8 +72,17 @@ console.log(question);
 console.log(Object.entries(openingHours));
 const hoursMap = new Map(Object.entries(openingHours));
 console.log(hoursMap);
-// *****Quiz app*****
 
+// *****Quiz app*****
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your Answer'));
+// const answer=3
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
 // *****Convert map to array*****
 
 /*
