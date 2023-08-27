c057cb3aee4e26229d5de667b6f9a3fb7fb0e88b
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-28 03:46:59 +0530

    MAP Fundamentals

--- script.js
+++ script.js
@@ -56,8 +56,37 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
-// *****Sets*****
+// *****Maps: Fundamentals*****
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon', 'Portugal'));
 
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarin', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < get.rest('close')));
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+///////////////////////////////////////
+// *****Sets*****
+/*
 const orderSet = new Set([
   'Pizza',
   'Pasta',
@@ -91,6 +120,8 @@ console.log(
 );
 
 console.log(new Set('khanfaizayub').size);
+*/
+
 ///////////////////////////////////////
 // Coding Challenge #2
 
