f2571d7366a73156a72bb9e199e46adfe001fe1c
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-08-28 03:01:10 +0530

    sets

--- script.js
+++ script.js
@@ -79,6 +79,18 @@ console.log(orderSet.delete('Risotto'));
 // orderSet.clear();
 console.log(orderSet);
 
+for (const order of orderSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('khanfaizayub').size);
 ///////////////////////////////////////
 // Coding Challenge #2
 
