ec572c3fc698779805060fe1027182d20ab88555
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-09-04 08:20:32 +0530

    String Methods Practice

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const openingHours = {
 };
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 
@@ -55,6 +55,32 @@ const restaurant = {
   },
 };
 
+/*
+///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replace(
+    '_'
+  )} from ${getCode(from)} to ${getCode(to)} ${time.replace(
+    ':',
+    'h'
+  )}`.padStart(50);
+  console.log(output);
+}
+*/
+
+/*
 ///////////////////////////////////////
 // Coding Challenge #4
 
@@ -83,6 +109,7 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'👍'.repeat(i + 1)}`);
   }
 });
+*/
 
 /*
 ///////////////////////////////////////
