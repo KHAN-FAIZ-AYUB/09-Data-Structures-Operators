b6b78164f44760f8637eb7740e674ff78ec30686
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-09-04 07:46:05 +0530

    Challenge #04

--- script.js
+++ script.js
@@ -56,6 +56,36 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Coding Challenge #4
+
+// underscore_case
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  // console.log(text);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    // console.log(first, second);
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'👍'.repeat(i + 1)}`);
+  }
+});
+
+/*
+///////////////////////////////////////
 // Working With Strings - Part 3
 //  *****Split and join*****
 console.log('a+very+nice+string'.split('+'));
@@ -102,7 +132,7 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(10);
 planesInLine(12);
-
+*/
 
 /*
 ///////////////////////////////////////
